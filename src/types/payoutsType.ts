6bb2b70bf07c9bc043bd69daf87c8f1b267e6920
Kartik Bhadouriya <ks8783375@gmail.com>
2026-04-30 09:09:04 +0530

    Update payoutsType.ts

--- src/types/payoutsType.ts
+++ src/types/payoutsType.ts
@@ -1,13 +1,34 @@
 
-import {type Seller } from "./sellerTypes";
-import {type Transaction } from "./Transaction";
+// import {type Seller } from "./sellerTypes";
+// import {type Transaction } from "./Transaction";
 
+
+// export interface Payouts {
+//   _id: number;
+//   transactions: Transaction[];
+//   seller: Seller;
+//   amount: number;
+//   status: "PENDING" | "SUCCESS" | "REJECTED";
+//   date: string;
+// }
+
+
+
+
+
+
+ import {type Seller } from "./sellerTypes";
+ import {type Transaction } from "./Transaction";
 
 export interface Payouts {
-  _id: number;
-  transactions: Transaction[];
+  _id: string;
   seller: Seller;
+  subOrder?: string | null;
   amount: number;
+  commissionAmount: number;
   status: "PENDING" | "SUCCESS" | "REJECTED";
-  date: string;
+  paidAt?: string | null;
+  createdAt: string;
+  updatedAt: string;
+  transactions?: Transaction[];
 }
