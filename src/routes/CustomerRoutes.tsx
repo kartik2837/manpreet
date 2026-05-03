4518e9052c42debb76defbf7f7dc31a1b01fd7c9
Kartik Bhadouriya <ks8783375@gmail.com>
2026-05-03 11:10:29 +0530

    Update CustomerRoutes.tsx

--- src/routes/CustomerRoutes.tsx
+++ src/routes/CustomerRoutes.tsx
@@ -65,6 +65,10 @@
 //       <Footer />
 //     </>
 
+//   )
+// }
+
+// export default CustomerRoutes
 
 
 
@@ -97,6 +101,7 @@ import SearchProducts from '../customer/pages/Search/SearchProducts'
 import { useEffect } from 'react'
 import FooterContent from '../customer/components/Footer/FooterContent'
 import Contact from '../customer/components/Footer/Contact'
+import DesktopPopup from '../customer/popup/DesktopPopup'
 
 
 const CustomerRoutes = () => {
@@ -116,7 +121,18 @@ const CustomerRoutes = () => {
       <Navbar />
 
       {/* ✅ Popup only once logic handled in component */}
-     
+      <DesktopPopup
+        initialImageUrl="https://cdn.vectorstock.com/i/1000v/19/10/cosmetics-beauty-products-for-make-up-sale-banner-vector-38391910.jpg"
+        allowedMobile="9992088843"
+        delay={10000}
+      />
+
+       <DesktopPopup
+  initialImageUrl="https://cdn.vectorstock.com/i/1000v/19/10/cosmetics-beauty-products-for-make-up-sale-banner-vector-38391910.jpg"
+  allowedMobile="9992088843" // sirf ye number se upload ho
+  delay={10000} // 10 sec
+  
+/>
 
 
       <Routes>
@@ -136,7 +152,7 @@ const CustomerRoutes = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/:slug" element={<FooterContent />} />
         <Route path='*' element={<NotFound />} />
-        <Route path='/product/:productId' element={<ProductDetails />} />     
+                <Route path='/product/:productId' element={<ProductDetails />} />
       </Routes>
 
       <Footer />
@@ -146,26 +162,3 @@ const CustomerRoutes = () => {
 
 export default CustomerRoutes
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   )
-// }
-
-// export default CustomerRoutes
